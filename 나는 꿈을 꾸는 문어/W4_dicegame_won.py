e20371aede5d8662799dc8e941b7a35d094af64e
dawonparkk <workpanda.n@gmail.com>
2021-04-18 19:56:22 +0900

    재귀함수_주사위 경우의수

--- 나는 꿈을 꾸는 문어/W4_dicegame_won.py
+++ 나는 꿈을 꾸는 문어/W4_dicegame_won.py
@@ -1,23 +1,12 @@
-# n = int(input())
-# dice = [0]*(n+1)
-
-# def dfs(depth):
-#     if depth>n:
-#         print(*dice[1:])
-#         return
-#     for i in range(1,7):
-#         dice[depth] = i
-#         dfs(depth+1)
-
-# dfs(1)
-
 n = int(input())
-arr = [0]*(n+1)
+dice = [0]*(n+1)
+
 def dfs(depth):
     if depth>n:
-        print(*arr[1:])
+        print(*dice[1:])
         return
     for i in range(1,7):
-        arr[depth]= i
+        dice[depth] = i
         dfs(depth+1)
+
 dfs(1)
