1cb91dc92e9a8544b1e5144747849cb8438f5e5d
YoonHeeMoon <yhm721@naver.com>
2021-08-28 13:50:39 +0900

    2021-08-28

--- react-study/src/Calendar.js
+++ react-study/src/Calendar.js
@@ -1,30 +1,37 @@
 import { useState } from "react";
 
 const Calendar = () => {
-
-    const yoyils = ["SUN","MON","TUE","WEN","THU","FRI","SAT"];
-
-
-    var days = []; // 1부터 31이 들어간 리스트를 만들거임
-    for(var i=1 ;i<=31;i++){
-        days.push(i)
-    }
-
-    var date = new Date("2021-10-23"); //오늘의 년-월-일(대한민국표준시) 
+    var date = new Date(); //오늘의 년-월-일(대한민국표준시) 
     const [today,setToday] = useState(date.getDate());
+    
+    //윤달...
+    const yoyils = ["SUN","MON","TUE","WEN","THU","FRI","SAT"];
+    var mDays = [31,28,31,30,31,30,31,31,30,31,30,31]
+
     var thisFirst = new Date(date.getFullYear(),date.getMonth(),1)
     //이번달 1일
-
     var ibundalililsyoyil = thisFirst.getDay();
     //이번달 1일의 요일 (일요일 = 0, 월요일 = 1 ...)
+    const thisMonth = date.getMonth();
+    const thisYear = date.getFullYear();
 
+    function isLeapYear(year) {
+        return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+    }
+    if(isLeapYear(thisYear)){
+        mDays[1]=29
+    }
+
+    var days = []; // 1부터 31이 들어간 리스트를 만들거임
+    for(var i=1 ;i<=mDays[thisMonth];i++){
+        days.push(i)
+    }
     function clickDate(i){
         setToday(i)
     }
-    
     return ( 
-        <div>
-
+        <div className="Calendar-container">
+            <div className="cal-month"> {thisMonth+1}월 </div>
             <div className="cal-con">
             {
                 yoyils.map((yoyil)=>
